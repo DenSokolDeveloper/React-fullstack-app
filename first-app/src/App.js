19b2985a65fa6e6a02a4be4c5049c1c6f490529f
Den <sokold3@gmail.com>
2023-06-15 13:23:04 +0300

    init

---
+++ first-app/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/api/');
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const jsonData = await response.json();
+      console.log(jsonData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={fetchData}>Send Request</button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
