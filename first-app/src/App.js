4d301cd31d04a136adcdaf68da7a002f725cfb52
Den <sokold3@gmail.com>
2023-06-15 14:02:07 +0300

    init

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const fetchData = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/v1/');
+      const response = await fetch('https://react-fullstack-app-api.vercel.app/api/v1/');
       if (!response.ok) {
         throw new Error('Failed to fetch data');
       }
